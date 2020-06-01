b16af783de8cec371c79a419a490d28a3b092924
MichalRozek98 <rozek.michal98@gmail.com>
2020-06-01 16:01:39 +0200

    Add Negative

--- SystemyWizyjne/Negative.Designer.cs
+++ SystemyWizyjne/Negative.Designer.cs
@@ -29,23 +29,128 @@
         private void InitializeComponent()
         {
             this.panel_alibaba = new System.Windows.Forms.Panel();
+            this.button_change_from_path = new System.Windows.Forms.Button();
+            this.label_negative = new System.Windows.Forms.Label();
+            this.label_original = new System.Windows.Forms.Label();
+            this.groupBox_negative = new System.Windows.Forms.GroupBox();
+            this.pictureBox_negative = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox_original = new System.Windows.Forms.GroupBox();
+            this.pictureBox_original = new System.Windows.Forms.PictureBox();
+            this.panel_alibaba.SuspendLayout();
+            this.groupBox_negative.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_negative)).BeginInit();
+            this.groupBox_original.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_original)).BeginInit();
             this.SuspendLayout();
             // 
             // panel_alibaba
             // 
+            this.panel_alibaba.Controls.Add(this.button_change_from_path);
+            this.panel_alibaba.Controls.Add(this.label_negative);
+            this.panel_alibaba.Controls.Add(this.label_original);
+            this.panel_alibaba.Controls.Add(this.groupBox_negative);
+            this.panel_alibaba.Controls.Add(this.checkBox1);
+            this.panel_alibaba.Controls.Add(this.groupBox_original);
             this.panel_alibaba.Location = new System.Drawing.Point(0, 0);
             this.panel_alibaba.Name = "panel_alibaba";
             this.panel_alibaba.Size = new System.Drawing.Size(930, 642);
             this.panel_alibaba.TabIndex = 0;
             // 
-            // Deep
+            // button_change_from_path
+            // 
+            this.button_change_from_path.BackColor = System.Drawing.Color.DarkGray;
+            this.button_change_from_path.FlatAppearance.BorderSize = 0;
+            this.button_change_from_path.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_change_from_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_change_from_path.Location = new System.Drawing.Point(379, 541);
+            this.button_change_from_path.Name = "button_change_from_path";
+            this.button_change_from_path.Size = new System.Drawing.Size(175, 69);
+            this.button_change_from_path.TabIndex = 14;
+            this.button_change_from_path.Text = "Change picture";
+            this.button_change_from_path.UseVisualStyleBackColor = false;
+            this.button_change_from_path.Click += new System.EventHandler(this.button_change_from_path_Click);
+            // 
+            // label_negative
+            // 
+            this.label_negative.AutoSize = true;
+            this.label_negative.Font = new System.Drawing.Font("Sitka Small", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_negative.Location = new System.Drawing.Point(631, 491);
+            this.label_negative.Name = "label_negative";
+            this.label_negative.Size = new System.Drawing.Size(141, 40);
+            this.label_negative.TabIndex = 13;
+            this.label_negative.Text = "Negative";
+            // 
+            // label_original
+            // 
+            this.label_original.AutoSize = true;
+            this.label_original.Font = new System.Drawing.Font("Sitka Small", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_original.Location = new System.Drawing.Point(165, 491);
+            this.label_original.Name = "label_original";
+            this.label_original.Size = new System.Drawing.Size(134, 40);
+            this.label_original.TabIndex = 12;
+            this.label_original.Text = "Original";
+            // 
+            // groupBox_negative
+            // 
+            this.groupBox_negative.Controls.Add(this.pictureBox_negative);
+            this.groupBox_negative.Location = new System.Drawing.Point(477, 33);
+            this.groupBox_negative.Name = "groupBox_negative";
+            this.groupBox_negative.Size = new System.Drawing.Size(440, 440);
+            this.groupBox_negative.TabIndex = 11;
+            this.groupBox_negative.TabStop = false;
+            // 
+            // pictureBox_negative
+            // 
+            this.pictureBox_negative.Location = new System.Drawing.Point(23, 28);
+            this.pictureBox_negative.Name = "pictureBox_negative";
+            this.pictureBox_negative.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox_negative.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_negative.TabIndex = 3;
+            this.pictureBox_negative.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(572, 218);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox_original
+            // 
+            this.groupBox_original.Controls.Add(this.pictureBox_original);
+            this.groupBox_original.Location = new System.Drawing.Point(14, 33);
+            this.groupBox_original.Name = "groupBox_original";
+            this.groupBox_original.Size = new System.Drawing.Size(440, 440);
+            this.groupBox_original.TabIndex = 9;
+            this.groupBox_original.TabStop = false;
+            // 
+            // pictureBox_original
+            // 
+            this.pictureBox_original.Location = new System.Drawing.Point(19, 28);
+            this.pictureBox_original.Name = "pictureBox_original";
+            this.pictureBox_original.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox_original.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_original.TabIndex = 0;
+            this.pictureBox_original.TabStop = false;
+            // 
+            // Negative
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.Controls.Add(this.panel_alibaba);
-            this.Name = "Deep";
+            this.Name = "Negative";
             this.Size = new System.Drawing.Size(930, 642);
+            this.panel_alibaba.ResumeLayout(false);
+            this.panel_alibaba.PerformLayout();
+            this.groupBox_negative.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_negative)).EndInit();
+            this.groupBox_original.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_original)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -53,5 +158,13 @@
         #endregion
 
         private System.Windows.Forms.Panel panel_alibaba;
+        private System.Windows.Forms.Button button_change_from_path;
+        private System.Windows.Forms.Label label_negative;
+        private System.Windows.Forms.Label label_original;
+        private System.Windows.Forms.GroupBox groupBox_negative;
+        private System.Windows.Forms.PictureBox pictureBox_negative;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox_original;
+        private System.Windows.Forms.PictureBox pictureBox_original;
     }
 }
--- SystemyWizyjne/Negative.cs
+++ SystemyWizyjne/Negative.cs
@@ -25,13 +25,57 @@ namespace SystemyWizyjne
             }
         }
 
+        private string path = @"..\..\..\Images\picture.png";
+
         public Negative()
         {
             InitializeComponent();
+            SetNegative();
+            button_change_from_path.Cursor = Cursors.Hand;
         }
 
-        private void panel_alibaba_Paint(object sender, PaintEventArgs e)
+        private void SetNegative()
         {
+            Bitmap bitmap = new Bitmap(path);
+            pictureBox_original.Image = Image.FromFile(path);
+
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+
+            Color p;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    p = bitmap.GetPixel(i, j);
+
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+
+                    r = 255 - r;
+                    g = 255 - g;
+                    b = 255 - b;
+
+                    bitmap.SetPixel(i, j, Color.FromArgb(a, r, g, b));
+                }
+            }
+            pictureBox_negative.Image = bitmap;
+        }
+
+        private void button_change_from_path_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PNG|*.png|Bitmaps|*.bmp|jpages|*.jpg";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+                pictureBox_original.Image = Image.FromFile(path);
+            }
+            SetNegative();
         }
     }
 }
